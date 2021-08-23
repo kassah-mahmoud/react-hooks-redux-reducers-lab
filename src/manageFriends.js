8176aee6969970ee21dd8f4facd6a768c75ffe04
Mahmoud <kassah.webdev@gmail.com>
2021-08-23 17:17:14 +0300

    Done.

--- src/manageFriends.js
+++ src/manageFriends.js
@@ -1,3 +1,18 @@
-export function manageFriends(state, action) {
-  // your code here
+export function manageFriends(state = {
+  friends: []
+}, action) {
+  switch (action.type) {
+    case "friends/add":
+      return {
+        ...state,
+        friends: [...state.friends, action.payload]
+      }
+    case "friends/remove":
+      return {
+        ...state,
+        friends: state.friends.filter(friend => friend.id !== action.payload)
+      }
+    default:
+      return state;
+  }
 }
--- src/managePresents.js
+++ src/managePresents.js
@@ -1,3 +1,13 @@
-export function managePresents(state, action) {
-  // your code here
+export function managePresents(state = {
+  numberOfPresents: 0
+}, action) {
+  switch (action.type) {
+    case "presents/increase":
+      return {
+        ...state,
+        numberOfPresents: state.numberOfPresents + 1
+      }  
+    default:
+      return state;
+  }
 }
